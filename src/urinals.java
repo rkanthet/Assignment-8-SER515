3646d49435b0afa0e0d52bc68e9ebc1131399ead
rahul <rkanthet@asu.edu>
2022-10-27 22:13:12 -0700

    one logic and one null testcase

--- src/urinals.java
+++ src/urinals.java
@@ -2,5 +2,37 @@
 public class urinals {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String a = countUrinals("11");
+        System.out.println(a);
     }
+
+    public static String countUrinals(String input){
+        if(input == null){
+            return "Input String Null";
+        }
+        int n = input.length();
+        int ans = 0;
+
+        for(int i=0;i < n;i++) {
+            if (input.charAt(i) == '1') {
+                if (input.charAt(i + 1) == '1') {
+                    ans = -1;
+                    break;
+                }
+            } else if (input.charAt(i) == '0') {
+
+                if ((i - 1 < 0 || input.charAt(i - 1) == '0') && (i + 1 >= n || input.charAt(i + 1) == '0'))
+                    ans++;
+                    i++;
+
+            }
+            else{
+                return "Invalid input";
+            }
+        }
+
+        return String.valueOf(ans);
+
+    }
+
 }
--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -1,2 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class urinalsTest {
+    // This Junit tests if the file exists or not
+    // This particular testcase checks if "urinals.dat" is found or not
+    urinals testcase = new urinals();
+
+//    @org.junit.jupiter.api.Test
+//    void fileexists() {
+//        assertEquals(1,testcase.Readfile("urinals.dat"));
+//    }
+//    // Misspelled the file name to show it does not exist
+//    @org.junit.jupiter.api.Test
+//    void fileDoesNotExists() {
+//        assertEquals(-1,testcase.Readfile("urvinals.dat"));
+//    }
+
+    @org.junit.jupiter.api.Test
+    void invalidInput(){
+        assertEquals("Input String Null",testcase.countUrinals(null));
+    }
+
+    @org.junit.jupiter.api.Test
+    void logicTest1(){
+        assertEquals("2",testcase.countUrinals("0000"));
+    }
 }
