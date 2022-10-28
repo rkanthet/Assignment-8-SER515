32cf0e6b7177f0790af6949f1c0e4778160034ba
rahul <rkanthet@asu.edu>
2022-10-27 22:18:16 -0700

    Adding more junit for countUrinals

--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -5,23 +5,18 @@ public class urinalsTest {
     // This particular testcase checks if "urinals.dat" is found or not
     urinals testcase = new urinals();
 
-//    @org.junit.jupiter.api.Test
-//    void fileexists() {
-//        assertEquals(1,testcase.Readfile("urinals.dat"));
-//    }
-//    // Misspelled the file name to show it does not exist
-//    @org.junit.jupiter.api.Test
-//    void fileDoesNotExists() {
-//        assertEquals(-1,testcase.Readfile("urvinals.dat"));
-//    }
-
     @org.junit.jupiter.api.Test
-    void invalidInput(){
+    void nullInput(){
         assertEquals("Input String Null",testcase.countUrinals(null));
     }
 
     @org.junit.jupiter.api.Test
     void logicTest1(){
         assertEquals("2",testcase.countUrinals("0000"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void invalidInput(){
+        assertEquals("Invalid input",testcase.countUrinals("aaabbb"));
     }
 }
